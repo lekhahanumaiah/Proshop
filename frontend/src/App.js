75336af2f4c668f211e36ab492ad63b8769b9119
lekhahanumaiah <lekhadh03@gmail.com>
2025-07-21 09:21:02 +0530

    changed the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my first e-Commerce website</h1>
-      <p>I hope it will go well!</p>
+      <p>Welcome to my first e-Commerce website</p>
+      
     </div>
   )
 }
